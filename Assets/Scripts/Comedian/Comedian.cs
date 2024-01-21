3a399a2877ec5c20ce61449ece489fa675d17d2e
Yehor <yeros05040504@gmail.com>
2024-01-21 19:12:52 +0200

    Added: laugh particles

--- Assets/Scripts/Comedian/Comedian.cs
+++ Assets/Scripts/Comedian/Comedian.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class Comedian : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _particles;
     private int funScale = 0;
+
     public bool TellJoke(Joke joke)
     {
         if (Random.value * 100 <= joke.FunLvl)
+        {
+            _particles.Play();
             return true;
+        }
         funScale += joke.FunLvl;
         if (Random.value * 100 <= funScale)
+        {
+            _particles.Play();
             return true;
+        }
         return false;
     }
 }
